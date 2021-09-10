d0faaae33913772c320e388dfcd8743f8bd41060
L4ZY91 <47037774+L4ZY91@users.noreply.github.com>
2021-09-09 22:16:54 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -21,69 +21,13 @@ xui.Class('App', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
             append(
-                xui.create("xui.UI.ButtonViews")
-                .setHost(host,"xui_ui_buttonviews3")
-                .setItems([
-                    {
-                        "id" : "a",
-                        "caption" : "Home",
-                        "imageClass" : "xui-icon-xui"
-                    },
-                    {
-                        "id" : "b",
-                        "caption" : "Penal Code"
-                    },
-                    {
-                        "id" : "c",
-                        "caption" : "page3"
-                    },
-                    {
-                        "id" : "d",
-                        "caption" : "page4",
-                        "closeBtn" : true,
-                        "optBtn" : "xui-uicmd-opt",
-                        "popBtn" : true
-                    }
-                ])
-                .setLeft("0em")
-                .setTop("0em")
-                .setBarLocation("left")
-                .setBarSize("10.666666666666666em")
-                .setValue("a")
-            );
-            
-            host.xui_ui_buttonviews3.append(
-                xui.create("xui.UI.Div")
-                .setHost(host,"xui_ui_div19")
-                .setLeft("-10.666666666666666em")
-                .setTop("1.5238095238095237em")
-                .setWidth("53.333333333333336em")
-                .setHeight("43.50476190476191em")
-                .setPosition("relative"),
-                "a"
-            );
-            
-            host.xui_ui_div19.append(
                 xui.create("xui.UI.Element")
-                .setHost(host,"xui_ui_element1")
+                .setHost(host,"xui_ui_element3")
                 .setLeft("0em")
-                .setTop("0em")
+                .setTop("22.095238095238095em")
                 .setWidth("52.95238095238095em")
-                .setHeight("45.180952380952384em")
-                .setHtml("https://usdojrpc.bubbleapps.io/")
-            );
-            
-            host.xui_ui_buttonviews3.append(
-                xui.create("xui.UI.Label")
-                .setHost(host,"xui_ui_label1")
-                .setDock("top")
-                .setLeft("0em")
-                .setTop("0em")
-                .setWidth("42.05714285714286em")
-                .setCaption("United States Department Of Justice CAD|MDT")
-                .setHAlign("left")
-                .setFontColor("#FFFFFF"),
-                "a"
+                .setHeight("23.847619047619048em")
+                .setHtml("<a href=\"https://usdojrpc.bubbleapps.io/\">https://usdojrpc.bubbleapps.io/</a>")
             );
             
             return children;
